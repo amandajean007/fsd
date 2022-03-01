6c1b0903f59177c5fef122d23fe8a19c863c1e0f
Amanda McBee <ahanes35@yahoo.com>
2022-02-28 20:56:05 -0700

    creting splash page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/weed.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <h3>
+          Frozen Smoke Dispensary
+        </h3>
+        <h4>
+          Are you over the age of 21?
+        </h4>
+        
+        <container>
+          <button>Yes</button>
+          <button>No</button>
+        </container>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
